88f1a4e5b9ff6fdf6edc7c31912eac336c4c70bb
bruno isaac sanches tavares <127205757+zexxx168@users.noreply.github.com>
2023-04-25 10:06:07 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -12,27 +12,30 @@ import "fmt"
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
 
 func ClassifyPrices(prices []int) (int, error) {
-	if len(prices) == 0{
-		return 0, fmt.Errorf("error")
+	if len(prices) == 0 {
+		return 0, fmt.Errorf("erro")
 	}
-	if len(prices) == 1{
-		return 3,nil
+	if len(prices) == 1 {
+		return 3, nil
 	}
-	var crescente bool
-	var decrescente bool
-	for i := 1;i < len(prices); i++{
-		if prices[i] < prices[i-1]{
+	var crescente bool = true
+	var decrescente bool = true
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < prices[i-1] {
 			crescente = false
-		}else if prices [i] > prices [i-1]{
+		} else if prices[i] > prices[i-1] {
 			decrescente = false
 		}
-		if crescente == true {
-			return 1, nil
-		}else if decrescente == true {
-			return 2, nil
-		}else {
-			return 3,nil
-		}
+
+	}
+	if crescente == true {
+		return 1, nil
+	} else if decrescente == true {
+		return 2, nil
+	} else {
+		return 3, nil
 	}
 	return 0, nil
+	return 0, nil
 }
+
