522857babe5856d65352b3ee278e7a5fd325044d
bruno isaac sanches tavares <127205757+zexxx168@users.noreply.github.com>
2023-04-25 09:36:04 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -14,9 +14,10 @@ package q2
 
 func ProblemsSolved(answers [][3]bool) int {
 	count := 0
+
 	for i := 0; i < len(answers); i++ {
 		soma := 0
-		for j := 0; i < 3; j++ {
+		for j := 0; j < 3; j++ {
 			if answers[i][j] {
 				soma++
 			}
